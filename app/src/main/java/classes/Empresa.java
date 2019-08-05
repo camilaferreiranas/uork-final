b6fdb94161386bbc67846719e65223714f20833b
Camila Ferreira <camila_fn97@hotmail.com>
2019-08-05 20:55:53 -0300

    Adição de activity de cadastro de pessoas.

--- app/src/main/java/classes/Empresa.java
+++ app/src/main/java/classes/Empresa.java
@@ -6,16 +6,36 @@ public class Empresa {
     private boolean empresa_assinante;
     private long cnpj_empresa;
     private String tipo_empresa;
+    private String email_empresa;
+    private String senha_empresa;
 
     public Empresa() {
     }
 
-    public Empresa(int id_empresa, String nome_empresa, boolean empresa_assinante, long cnpj_empresa, String tipo_empresa) {
+    public Empresa(int id_empresa, String nome_empresa, boolean empresa_assinante, long cnpj_empresa, String tipo_empresa, String email_empresa, String senha_empresa) {
         this.id_empresa = id_empresa;
         this.nome_empresa = nome_empresa;
         this.empresa_assinante = empresa_assinante;
         this.cnpj_empresa = cnpj_empresa;
         this.tipo_empresa = tipo_empresa;
+        this.email_empresa = email_empresa;
+        this.senha_empresa = senha_empresa;
+    }
+
+    public String getEmail_empresa() {
+        return email_empresa;
+    }
+
+    public void setEmail_empresa(String email_empresa) {
+        this.email_empresa = email_empresa;
+    }
+
+    public String getSenha_empresa() {
+        return senha_empresa;
+    }
+
+    public void setSenha_empresa(String senha_empresa) {
+        this.senha_empresa = senha_empresa;
     }
 
     public int getId_empresa() {
---
+++ app/src/main/java/com/uork/uork/CadastroEmpresa.java
@@ -0,0 +1,13 @@
+package com.uork.uork;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroEmpresa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_empresa);
+    }
+}
