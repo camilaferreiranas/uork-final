b7bdebdc345fe632503333fa550ba940f50d112f
Camila Ferreira <camila_fn97@hotmail.com>
2019-08-22 19:32:51 -0300

    Login com email completo.

--- app/src/main/java/com/uork/uork/LoginActivity.java
+++ app/src/main/java/com/uork/uork/LoginActivity.java
@@ -4,12 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -79,9 +86,46 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             Intent intent = new Intent(this, CadastroUsuario.class);
             startActivity(intent);
         } else if(v.getId()== R.id.loginFacebook) {
-
+            //ações facebook
         } else if(v.getId() == R.id.loginGoogle) {
+            //ações google
+        } else if(v.getId() == R.id.login) {
+            //ações auth email
+            loginUsuarioEmail();
 
         }
     }
+
+    private void loginUsuarioEmail() {
+        String email, senha;
+        email = username.getText().toString();
+        senha = password.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Todos campos devem ser preenchidos", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(senha)) {
+            Toast.makeText(getApplicationContext(), "Todos campos devem ser preenchidos", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+
+        mAuth.signInWithEmailAndPassword(email,senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Bem vindo!", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Login inválido.Cheque suas informações.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
+
+
 }
